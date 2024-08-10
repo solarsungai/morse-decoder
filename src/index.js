3fc7bceabb3a04096d858ce731ca425b6a60f92d
solarsungai <solarsungai@gmail.com>
2024-08-10 18:14:28 +0300

    feat: solved the task

--- src/index.js
+++ src/index.js
@@ -38,8 +38,47 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+
+   let oneZeroArray = [];
+   let oneZeroLetterArray = [];
+   let dotsDashesArray = [];
+   let letterArray = [];
+
+   let n = 0;
+   while (n < expr.length) {
+   oneZeroArray.push(expr.slice(n, n + 10));
+   n = n + 10; };
+
+   let m = 0;
+   while (m < oneZeroArray.length) {
+   let a = 0;
+   while (a <= 10) {
+   if (oneZeroArray[m].slice(a, a + 2) === '10') {
+   oneZeroLetterArray.push('.');
+   } else if (oneZeroArray[m].slice(a, a + 2) === '11') {
+    oneZeroLetterArray.push('-');
+   } else if (oneZeroArray[m].slice(a, a + 2) === '**') {
+    oneZeroLetterArray.push('');}
+    a = a + 2;
+   };
+   dotsDashesArray.push(oneZeroLetterArray.join(''));
+   oneZeroLetterArray.length = 0;
+   m++;
+   };
+
+   let b = 0;
+   while (b < dotsDashesArray.length) {
+   if (dotsDashesArray[b] === '') {
+   letterArray.push(' ');
+   } else {
+   letterArray.push(MORSE_TABLE[dotsDashesArray[b]]);
+   }
+   b++;
+   };
+
+   return letterArray.join('');
+
+};
 
 module.exports = {
     decode
